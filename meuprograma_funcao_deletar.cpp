188f94f169ad55d80572399ec252d7f75b46e63b
luanagil-source <luanagilduarte@gmail.com>
2025-05-15 11:07:14 -0300

    Add files via upload

--- meuprograma_funcao_deletar.cpp
+++ meuprograma_funcao_deletar.cpp
@@ -130,6 +130,7 @@ int consulta ()//Função responsável por consultar os usuários no Sistema
 		printf("\t1- Registrar nomes\n");
 		printf("\t2- Consultar nomes\n");
 		printf("\t3- Deletar nomes\n\n");
+		printf("\t4- Sair do sistema\n\n");
 		printf("opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -149,6 +150,11 @@ int consulta ()//Função responsável por consultar os usuários no Sistema
 			
 			case 3:
 			deletar(); //chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
